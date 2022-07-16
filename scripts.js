36b9734689d678206c2496b109b413f9c19394a6
Carlos Henrique <carloshenriquepvh@hotmail.com>
2022-07-16 13:45:29 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("btn-blue").
+addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "blue");
+})
+
+document.getElementById("btn-vermelho").
+addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "vermelho");
+})
+
+document.getElementById("btn-amarelo").
+addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "amarelo");
+})
